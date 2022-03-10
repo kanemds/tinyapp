038b1e0da55834947264a52c09355f6b4925d2da
Kanem <kanemdeng@email.com>
2022-03-09 21:35:46 -0800

    added urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set("view engine". ejs);
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,12 +13,26 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls:urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
