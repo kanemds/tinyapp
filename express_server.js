4bbd63a97b9d6ffbe545f1a6c0c5684998676af2
Kanem <kanemdeng@email.com>
2022-03-10 20:05:26 -0800

    added generateRandomString and body parser and urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+const generateRandomString = () => {
+  const string = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let url = "";
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * string.length);
+    url += string[index];
+  }
+  return url;
+};
+
+console.log(generateRandomString());
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 
@@ -34,6 +58,8 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
+
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
