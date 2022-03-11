fb2af6dd320af942f8700c6b7a50e99f964173b2
Kanem <kanemdeng@email.com>
2022-03-11 09:27:24 -0800

    added nodemon and json common line

--- express_server.js
+++ express_server.js
@@ -20,7 +20,7 @@ console.log(generateRandomString());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.post("/urls", (req, res) => {
